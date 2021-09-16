f62598811efe25e4643f6c29d60bf564092a940f
Eugenia Alcaraz <eugeniaalcaraz95@gmail.com>
2021-09-16 16:13:34 -0300

    Cambios euge 16/Setiembre

--- BD2/QueryBDobl.sql
+++ BD2/QueryBDobl.sql
@@ -1,5 +1,7 @@
 USE master
 
+CREATE DATABASE BD2Obligatorio
+
 USE BD2Obligatorio
 
 CREATE TABLE TREN
@@ -11,6 +13,7 @@ CREATE TABLE TREN
 	CONSTRAINT UK_LetraTren UNIQUE (Letra)
 )
 
+
 CREATE TABLE VAGON
 (
 	CodigoVagon numeric(5) not null,
@@ -20,6 +23,7 @@ CREATE TABLE VAGON
 	CONSTRAINT FEFK_VagonNumTren FOREIGN KEY(NumeroTren) REFERENCES TREN(NumeroTren),
 	CONSTRAINT CK_CapacidadVagon CHECK(Capacidad <= 40)
 )
+CREATE INDEX IDX_NumeroTren on VAGON(NumeroTren)
 
 CREATE TABLE ESTACION
 (
@@ -47,6 +51,7 @@ CREATE TABLE LINEA
 	CONSTRAINT FK_LineaCodColor FOREIGN KEY(CodigoColor) REFERENCES COLOR (CodigoColor),
 	CONSTRAINT CK_LongitudLinea CHECK (Longitud >= 1)
 )
+CREATE INDEX IDX_LineaCodColor on LINEA(CodigoColor)
 
 CREATE TABLE LINEA_ESTACION
 (
@@ -58,36 +63,45 @@ CREATE TABLE LINEA_ESTACION
 	CONSTRAINT FK_LineaEstacionNumLinea FOREIGN KEY(NumeroLinea) REFERENCES LINEA (NumeroLinea),
 	CONSTRAINT UK_ExClavePrimariaLineaEstacion UNIQUE (CodigoEstacion, NumeroLinea)
 )
+CREATE INDEX IDX_LineaEstacionCodEstacion on LINEA_ESTACION(CodigoEstacion)
+CREATE INDEX IDX_LineaEstacionNumLinea on LINEA_ESTACION(NumeroLinea)
 
 CREATE TABLE TREN_LINEA_ESTACION
 (
-	IdTrenLineaEstacion NUMERIC(5) NOT NULL Identity(1,1),
-	NumeroTren NUMERIC(5) NOT NULL,
-	IdEstacionLinea NUMERIC NOT NULL,
-	Fecha DATE NOT NULL,
-	Hora TIME NOT NULL,
+	IdTrenLineaEstacion numeric(5) not null Identity(1,1),
+	NumeroTren numeric(5) not null,
+	IdLineaEstacion numeric not null,
+	Fecha smalldatetime not null,
+
 	CONSTRAINT PK_TrenLineaEstacion PRIMARY KEY (IdTrenLineaEstacion),
 	CONSTRAINT FK_Tren FOREIGN KEY (NumeroTren) REFERENCES Tren (NumeroTren),
-	CONSTRAINT UK_ExClavePrimariaTrenLineaEstacion UNIQUE (NumeroTren, IdEstacionLinea)
+	CONSTRAINT FK_EstacionLinea FOREIGN KEY (IdLineaEstacion) REFERENCES LINEA_ESTACION (IdLineaEstacion),
+	CONSTRAINT UK_ExClavePrimariaTrenLineaEstacion UNIQUE (NumeroTren, IdLineaEstacion)
 )
+CREATE INDEX IDX_Tren on TREN_LINEA_ESTACION(NumeroTren)
+CREATE INDEX IDX_EstacionLinea on TREN_LINEA_ESTACION(IdLineaEstacion)
 
 CREATE TABLE DESTINO
 (
-	NumeroLinea NUMERIC(5) NOT NULL,
-	CodigoEstacion NUMERIC(5) NOT NULL,
+	NumeroLinea numeric(5) not null,
+	CodigoEstacion numeric(5) not null,
 	CONSTRAINT PK_Destino PRIMARY KEY (NumeroLinea),
 	CONSTRAINT FK_DestinoNumeroLinea FOREIGN KEY (NumeroLinea) REFERENCES LINEA (NumeroLinea),
 	CONSTRAINT FK_DestinoCodigoEstacion FOREIGN KEY (CodigoEstacion) REFERENCES ESTACION (CodigoEstacion)
 )
+CREATE INDEX IDX_DestinoNumeroLinea on DESTINO(NumeroLinea)
+CREATE INDEX IDX_DestinoCodigoEstacion on DESTINO(CodigoEstacion)
 
 CREATE TABLE ORIGEN
 (
-	NumeroLinea NUMERIC(5) NOT NULL,
-	CodigoEstacion NUMERIC(5) NOT NULL,
+	NumeroLinea numeric(5) not null,
+	CodigoEstacion numeric(5) not null,
 	CONSTRAINT PK_Origen PRIMARY KEY (NumeroLinea),
 	CONSTRAINT FK_OrigenNumeroLinea FOREIGN KEY (NumeroLinea) REFERENCES Linea (NumeroLinea),
 	CONSTRAINT FK_ORIGEN_CODIGOESTACION FOREIGN KEY (CodigoEstacion) REFERENCES Estacion (CodigoEstacion)
 )
+CREATE INDEX IDX_OrigenNumeroLinea on ORIGEN(NumeroLinea)
+CREATE INDEX IDX_OrigenCodigoEstacion on ORIGEN(CodigoEstacion)
 
 SELECT
 	T.*
@@ -95,10 +109,10 @@ FROM
 	Tren T,
 	Tren_Linea_Estacion TLE
 WHERE
-T.NumeroTren = TLE.NumeroTren AND TLE.IdEstacionLinea = 1
+T.NumeroTren = TLE.NumeroTren AND TLE.IdLineaEstacion = 1
 
 
-ALTER TABLE TREN_LINEA_ESTACION DROP COLUMN FECHA;
+/*ALTER TABLE TREN_LINEA_ESTACION DROP COLUMN FECHA;
 ALTER TABLE TREN_LINEA_ESTACION DROP COLUMN HORA;
 
 
@@ -110,3 +124,4 @@ ALTER TABLE TREN_LINEA_ESTACION DROP COLUMN FechaHora;
 ALTER TABLE TREN_LINEA_ESTACION ADD FechaHora smalldatetime not null;
 
 delete from TREN_LINEA_ESTACION WHERE TREN_LINEA_ESTACION.IdTrenLineaEstacion = 1
+*/
